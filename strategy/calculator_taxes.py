df52bdc4144cde4f8031f38f23f1c49840c007f2
neldevfull <nfrugeri@gmail.com>
2016-05-21 20:18:14 +0000

    Initial commit with strategy design pattern

---
+++ strategy/calculator_taxes.py
@@ -0,0 +1,18 @@
+from taxes import ISS, ICMS
+
+
+class CalculatorTaxes(object):
+
+    def calculates(self, budget, taxe):
+
+        print(taxe.calc(budget))
+
+
+if __name__ == '__main__':
+    from budget import Budget
+
+    calculator = CalculatorTaxes()
+    budget     = Budget(500)
+
+    calculator.calculates(budget, ISS())
+    calculator.calculates(budget, ICMS())
---
+++ strategy/taxes.py
@@ -0,0 +1,9 @@
+class ISS(object):
+
+    def calc(self, budget):
+        return budget.value * .1
+
+class ICMS(object):
+
+    def calc(self, budget):
+        return budget.value * .06
